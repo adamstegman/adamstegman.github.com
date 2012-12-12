f7ffa988b3c779ee337535d2e943ee48856c6402
Adam Stegman <astegman@gmail.com>
2012-12-11 22:12:28 -0600

    Add adamstegman/adamstegman content

--- lib/default.rb
+++ lib/default.rb
@@ -3,4 +3,7 @@
 #
 # http://nanoc.stoneship.org/docs/4-basic-concepts/#helpers
 
+require File.expand_path(File.dirname(__FILE__)) + '/projects'
+
 include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Projects
