e368ed1f7fc222b203537c583ea4b220c59939f5
Adam Stegman <astegman@gmail.com>
2013-03-02 10:03:03 -0600

    Use nanoc's built-in partials.
    Rolled my own before I realized it had them.

--- lib/default.rb
+++ lib/default.rb
@@ -6,11 +6,9 @@
 # http://nanoc.stoneship.org/docs/4-basic-concepts/#helpers
 
 require File.expand_path(File.dirname(__FILE__)) + '/text_helpers'
-require File.expand_path(File.dirname(__FILE__)) + '/partials'
 require File.expand_path(File.dirname(__FILE__)) + '/projects'
 
 include Nanoc3::Helpers::TextHelpers
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Projects
-include Nanoc3::Helpers::Partials
