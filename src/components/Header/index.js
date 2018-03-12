aebe1164eca6f6f4148b8d79a6e6e5b07217b7db
Adam Stegman <me@adamstegman.com>
2018-03-11 23:31:34 -0700

    Use menu for navigation on small screens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link';
 
 import Atom from '../../assets/atom.svg';
 import Container from '../Container';
+import NavList from '../NavList';
 import resume from '../../assets/resume.pdf';
 import styles from './index.module.css';
 
@@ -23,28 +24,14 @@ class Header extends React.Component {
             </h1>
             <h2 className={styles.h2}>Senior Software Engineer</h2>
           </hgroup>
-          <nav>
-            <ul className={styles['nav-list']}>
-              <li className={styles['nav-item']}>
-                <Link to="/" className={styles['nav-link']}>About Me</Link>
-              </li>
-              <li className={styles['nav-item']}>
-                <a href="//blog.adamstegman.com" className={styles['nav-link']}>My Kind of Stupid (Blog)</a>
-              </li>
-              <li className={styles['nav-item']}>
-                <Link to="/projects" className={styles['nav-link']}>Projects</Link>
-              </li>
-              <li className={styles['nav-item']}>
-                <a href={resume} className={styles['nav-link']}>Résumé</a>
-              </li>
-              <li className={styles['nav-item']}>
-                <a href="//twitter.com/adamstegman" className={styles['nav-link']}>Twitter</a>
-              </li>
-              <li className={styles['nav-item']}>
-                <a href="//github.com/adamstegman" className={styles['nav-link']}>Github</a>
-              </li>
-            </ul>
-          </nav>
+          <NavList>
+            <Link to="/" className={styles['nav-link']}>About Me</Link>
+            <a href="//blog.adamstegman.com" className={styles['nav-link']}>My Kind of Stupid (Blog)</a>
+            <Link to="/projects" className={styles['nav-link']}>Projects</Link>
+            <a href={resume} className={styles['nav-link']}>Résumé</a>
+            <a href="//twitter.com/adamstegman" className={styles['nav-link']}>Twitter</a>
+            <a href="//github.com/adamstegman" className={styles['nav-link']}>Github</a>
+          </NavList>
         </Container>
       </header>
     );
