13a1b29cbfc02e634b1701b00af67a2bd4ec8e2f
Adam Stegman <me@adamstegman.com>
2022-04-25 23:16:06 -0500

    Updates

---
+++ component---src-pages-404-js-4d638f957a792b8dc377.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkAdamStegman_com=self.webpackChunkAdamStegman_com||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294);t.default=function(){return a.createElement("div",null,a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4d638f957a792b8dc377.js.map
